b6a058ea8b814ed2635947a9096d24307d99e9cf
Diana Kosta <dianakosta@gmail.com>
2022-02-22 21:12:42 -0500

    prerrier code

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -8,57 +8,76 @@ import {
   TextField,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { Quote, Theme, SymbolArr, getRoundVal, convToInterCurrency } from "../types/quote";
+import {
+  Quote,
+  Theme,
+  SymbolArr,
+  getRoundVal,
+  convToInterCurrency,
+} from "../types/quote";
 import moment from "moment-timezone";
 import { StyledGrid, StyledTypography } from "./style";
 import { getQuote } from "../API/quote";
 
-    const initialState: Quote = {
-    Name: "",
-    Symbol: "",
-    LastPrice: 0,
-    Change: 0,
-    ChangePercent: 0,
-    Timestamp: new Date(),
-    Low: 0,
-    High: 0,
-    Open: 0,
-    Volume: 0, //M
-    MarketCap: null, //B
-    };
+const initialState: Quote = {
+  Name: "",
+  Symbol: "",
+  LastPrice: 0,
+  Change: 0,
+  ChangePercent: 0,
+  Timestamp: new Date(),
+  Low: 0,
+  High: 0,
+  Open: 0,
+  Volume: 0, //M
+  MarketCap: null, //B
+};
 
 const Home = () => {
+  useEffect(() => {
+    CallOut(SymbolArr[0].label);
+  }, []);
 
-  useEffect(()=>{
-    CallOut(SymbolArr[0].label); 
-  },[])
-  
   const [quote, setQuote] = useState(initialState);
-  const handleInputChange = (event:  React.FormEvent<EventTarget>, value: string) => {
-    CallOut(value); 
-  }
-    const CallOut = async(value: string) =>  {
-      const responsePromise = await getQuote(value);
-      //console.log(responsePromise.data);
-      setQuote(responsePromise.data);
-    }    
-  
+  const handleInputChange = (
+    event: React.FormEvent<EventTarget>,
+    value: string
+  ) => {
+    CallOut(value);
+  };
+  const CallOut = async (value: string) => {
+    const responsePromise = await getQuote(value);
+    //console.log(responsePromise.data);
+    setQuote(responsePromise.data);
+  };
+
   return (
     <Paper sx={{ width: "35%", p: 4 }}>
       <Grid container>
-        <Grid item display="flex"
-            justifyContent="start"
-            alignItems="center"  xs={12} sx={{ borderBottom: `2px solid ${Theme.mainColor} `, py: 4 }}>
-            <Autocomplete
-                aria-label="input"
-                sx={{width: "50%"}}
-                options={SymbolArr.map((option) => option.label)}
-                onInputChange={(e:React.FormEvent<EventTarget> ,v: string) => handleInputChange(e,v)}
-                renderInput={(params) => 
-                    <TextField {...params} aria-label="Symbol lookup" label="Symbol lookup" 
-                       />}
-            />
-          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search" >
+        <Grid
+          item
+          display="flex"
+          justifyContent="start"
+          alignItems="center"
+          xs={12}
+          sx={{ borderBottom: `2px solid ${Theme.mainColor} `, py: 4 }}
+        >
+          <Autocomplete
+            aria-label="input"
+            sx={{ width: "50%" }}
+            options={SymbolArr.map((option) => option.label)}
+            onInputChange={(e: React.FormEvent<EventTarget>, v: string) =>
+              handleInputChange(e, v)
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                aria-label="Symbol lookup"
+                label="Symbol lookup"
+              />
+            )}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
             <SearchIcon />
           </IconButton>
         </Grid>
@@ -84,20 +103,24 @@ const Home = () => {
           <StyledTypography
             paddingRight={"5px"}
             fontSize={"20px"}
-            color={`${quote.Change > 0 ?Theme.positiveColor: Theme.negativeColor}`}
-          >
-            {`${quote.Change > 0 ? "+" : ""}${
-              getRoundVal(quote.Change)
+            color={`${
+              quote.Change > 0 ? Theme.positiveColor : Theme.negativeColor
             }`}
+          >
+            {`${quote.Change > 0 ? "+" : ""}${getRoundVal(quote.Change)}`}
           </StyledTypography>
           <StyledTypography
             paddingRight={"5px"}
             fontSize={"20px"}
-            color={`${quote.ChangePercent > 0 ?Theme.positiveColor: Theme.negativeColor}`}
+            color={`${
+              quote.ChangePercent > 0
+                ? Theme.positiveColor
+                : Theme.negativeColor
+            }`}
           >
-            {`(${
-              quote.ChangePercent > 0 ? "+" : ""
-            }${getRoundVal(quote.ChangePercent)})%`}
+            {`(${quote.ChangePercent > 0 ? "+" : ""}${getRoundVal(
+              quote.ChangePercent
+            )})%`}
           </StyledTypography>
         </Grid>
         <Grid item xs={12}>
@@ -122,11 +145,17 @@ const Home = () => {
             <StyledTypography color={Theme.mainColor}>Valume</StyledTypography>
             <StyledTypography fontWeight={"600"}>
               {convToInterCurrency(quote.Volume)}
-            </StyledTypography> 
+            </StyledTypography>
           </StyledGrid>
 
-          <StyledGrid item xs={12} sx={{ borderBottom: `1px solid ${Theme.mainColor}` }}>
-            <StyledTypography color={Theme.mainColor}>Market Cap</StyledTypography>
+          <StyledGrid
+            item
+            xs={12}
+            sx={{ borderBottom: `1px solid ${Theme.mainColor}` }}
+          >
+            <StyledTypography color={Theme.mainColor}>
+              Market Cap
+            </StyledTypography>
             <StyledTypography fontWeight={"600"}>
               {convToInterCurrency(quote.MarketCap)}
             </StyledTypography>
--- src/types/quote.ts
+++ src/types/quote.ts
@@ -1,62 +1,54 @@
 export interface Quote {
-    Name: string,
-    Symbol: string,
-    LastPrice: number,
-    Change:number,
-    ChangePercent: number,
-    Timestamp: Date,
-    Low: number,
-    High: number,
-    Open: number,
-    Volume: number,
-    MarketCap: number | null
+  Name: string;
+  Symbol: string;
+  LastPrice: number;
+  Change: number;
+  ChangePercent: number;
+  Timestamp: Date;
+  Low: number;
+  High: number;
+  Open: number;
+  Volume: number;
+  MarketCap: number | null;
 }
 
-
-export const getRoundVal = (numb:number) => {
-  if(!numb) return null;
+export const getRoundVal = (numb: number) => {
+  if (!numb) return null;
   return Math.round((numb + Number.EPSILON) * 100) / 100;
- 
-}
+};
 
-export const convToInterCurrency =  (labelValue: number | null) => {
-   if(!labelValue) return null;
+export const convToInterCurrency = (labelValue: number | null) => {
+  if (!labelValue) return null;
   // Nine Zeroes for Billions
-  return Math.abs(Number(labelValue)) >= 1.0e+9
+  return Math.abs(Number(labelValue)) >= 1.0e9
+    ? (Math.abs(Number(labelValue)) / 1.0e9).toFixed(2) + "B"
+    : // Six Zeroes for Millions
+    Math.abs(Number(labelValue)) >= 1.0e6
+    ? (Math.abs(Number(labelValue)) / 1.0e6).toFixed(2) + "M"
+    : // Three Zeroes for Thousands
+    Math.abs(Number(labelValue)) >= 1.0e3
+    ? (Math.abs(Number(labelValue)) / 1.0e3).toFixed(2) + "K"
+    : Math.abs(Number(labelValue));
+};
 
-  ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
-  // Six Zeroes for Millions 
-  : Math.abs(Number(labelValue)) >= 1.0e+6
+export const SymbolArr = [
+  { label: "MSFT", value: "MSFT" },
+  { label: "AAPL", value: "AAPL" },
+  { label: "INFO", value: "INFO" },
+  { label: "BRKA", value: "BRKA" },
+  { label: "F", value: "F" },
+  { label: "PLT", value: "PLT" },
+  { label: "BIG", value: "BIG" },
+  { label: "TWX", value: "TWX" },
+  { label: "AME", value: "AME" },
+  { label: "JWN", value: "JWN" },
+  { label: "CVS", value: "CVS" },
+  { label: "MS", value: "MS" },
+  { label: "MET", value: "MET" },
+];
 
-  ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
-  // Three Zeroes for Thousands
-  : Math.abs(Number(labelValue)) >= 1.0e+3
-
-  ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
-
-  : Math.abs(Number(labelValue));
-
-}
-
-
-export const SymbolArr =  [
-    {label:"MSFT", value:"MSFT"},
-    {label:"AAPL", value:"AAPL"},
-    {label:"INFO",value:"INFO"},
-    {label:"BRKA", value:"BRKA"},
-    {label:"F", value:"F"},
-    {label:"PLT", value:"PLT"},
-    {label:"BIG", value:"BIG"},
-    {label:"TWX", value:"TWX"},
-    {label:"AME", value:"AME"},
-    {label:"JWN", value:"JWN"},
-    {label:"CVS", value:"CVS"},
-    {label:"MS", value:"MS"},
-    {label:"MET", value:"MET"}
-]
-
-  export const Theme =  {
-    mainColor:"#C0C0C0",
-    positiveColor:"#228B22",
-    negativeColor:"red"
-  }
+export const Theme = {
+  mainColor: "#C0C0C0",
+  positiveColor: "#228B22",
+  negativeColor: "red",
+};
